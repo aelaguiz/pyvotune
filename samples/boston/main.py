12daf19361c092eb5561251ff0a2189670732c8c
aelaguiz@gmail.com <aelaguiz@gmail.com>
2012-12-27 08:51:21 -0600

    Boston sample working just by guessing params

--- samples/boston/main.py
+++ samples/boston/main.py
@@ -5,7 +5,6 @@ import sys
 
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.neighbors import KNeighborsRegressor
-from sklearn.linear_model import SGDRegressor
 from sklearn.cross_validation import train_test_split
 from sklearn.pipeline import Pipeline
 import sklearn.datasets
@@ -14,30 +13,23 @@ pyvotune.dense_input(RandomForestRegressor)
 pyvotune.terminal(RandomForestRegressor)
 pyvotune.pint(range=(1, 1000), name='n_estimators')(RandomForestRegressor)
 pyvotune.pfloat(range=(0, 1), name='min_density')(RandomForestRegressor)
-pyvotune.pconst(value=8, name='n_jobs')(RandomForestRegressor)
+pyvotune.pconst(value=1, name='n_jobs')(RandomForestRegressor)
 pyvotune.pbool(name='bootstrap')(RandomForestRegressor)
 
 pyvotune.dense_input(GradientBoostingRegressor)
 pyvotune.terminal(GradientBoostingRegressor)
 pyvotune.pint(range=(10, 5000), name='n_estimators')(GradientBoostingRegressor)
 pyvotune.pint(range=(1, 13), name='max_features')(GradientBoostingRegressor)
-pyvotune.pfloat(range=(0, 1), name='learn_rate')(GradientBoostingRegressor)
+pyvotune.pfloat(range=(0, 1), name='learning_rate')(GradientBoostingRegressor)
 pyvotune.pfloat(range=(0, 1), name='subsample')(GradientBoostingRegressor)
 
 pyvotune.dense_input(KNeighborsRegressor)
 pyvotune.terminal(KNeighborsRegressor)
-pyvotune.pint(range=(1, 100), name='n_neighbors')(KNeighborsRegressor)
+pyvotune.pint(range=(2, 100), name='n_neighbors')(KNeighborsRegressor)
 pyvotune.choice(choices=['uniform', 'distance'], name='weights')(KNeighborsRegressor)
 pyvotune.choice(choices=['auto', 'ball_tree', 'kd_tree', 'brute'], name='algorithm')(KNeighborsRegressor)
 pyvotune.pint(range=(10, 100), name='leaf_size')(KNeighborsRegressor)
 pyvotune.pint(range=(1, 20), name='p')(KNeighborsRegressor)
-
-#pyvotune.dense_input(SGDRegressor)
-pyvotune.terminal(SGDRegressor)
-#pyvotune.pfloat(range=(0, 0.2), name='alpha')(SGDRegressor)
-#pyvotune.pfloat(range=(0, 1), name='rho')(SGDRegressor)
-#pyvotune.pfloat(range=(0, 1), name='eta0')(SGDRegressor)
-#pyvotune.pint(range=(1, 20), name='n_iter')(SGDRegressor)
 
 
 def generator(random, args):
@@ -48,31 +40,55 @@ def generator(random, args):
 
 @inspyred.ec.evaluators.evaluator
 def evaluator(candidate, args):
-    if not candidate.assemble():
+    individual = train_candidate(
+        candidate, args['train_X'], args['train_y'])
+
+    if not individual:
         return sys.maxint
 
-    print "Testing", candidate.assembled
-    #try:
+    return test_individual(
+        individual, args['test_X'], args['test_y'])
+
+
+def train_candidate(candidate, train_X, train_y):
+    if not candidate.assemble():
+        return
+
     pipeline = Pipeline([
         (str(i), s) for i, s in enumerate(candidate.assembled)])
 
-    pipeline.fit(args['train_X'], args['train_y'])
+    pipeline.fit(train_X, train_y)
 
-    observed_y = pipeline.predict(args['test_X'])
+    return pipeline
+
+
+def test_individual(pipeline, test_X, test_y, display=False):
+    observed_y = pipeline.predict(test_X)
 
     mse = sklearn.metrics.mean_squared_error(
-        args['test_y'], observed_y)
+        test_y, observed_y)
 
-    print "Error", mse
+    if display:
+        total_err = 0
+        total_actual = 0
+        print "  #", "Actual", "Observed", "Err %"
+        print "---", "------", "--------", "-----"
+        for i, (actual, observed) in enumerate(zip(test_y, observed_y)):
+            err = abs(observed - actual)
+            err_pct = round(err / actual * 100., 2)
+
+            total_err += err
+            total_actual += actual
+
+            print str(i).zfill(3), str(actual).ljust(6), str(observed).ljust(8), str(err_pct).ljust(4)
+        print "MSE:", mse
+        print "Avg Err %:", round(total_err / total_actual * 100., 2)
 
     return mse
-    #except Exception as e:
-        #print e
-        #return sys.maxint
 
 
 if __name__ == '__main__':
-    pyvotune.set_debug(True)
+    pyvotune.set_debug(False)
 
     data = sklearn.datasets.load_boston()
     X = data['data']
@@ -86,10 +102,10 @@ if __name__ == '__main__':
             'sparse': False
         },
         gene_pool=[
-            RandomForestRegressor,
             GradientBoostingRegressor,
+            RandomForestRegressor,
             KNeighborsRegressor],
-#SGDRegressor, 
+
         max_length=1,
         noop_frequency=0.0)
 
@@ -110,8 +126,11 @@ if __name__ == '__main__':
 
     final_pop = ea.evolve(
         generator=generator,
-        evaluator=evaluator,
+        evaluator=inspyred.ec.evaluators.parallel_evaluation_mp,
         pyvotune_generator=gen,
+
+        mp_evaluator=evaluator,
+        mp_nprocs=8,
 
         #tolerance=0.25,
         max_time=10,
@@ -121,8 +140,13 @@ if __name__ == '__main__':
         test_X=test_X,
         test_y=test_y,
 
-        pop_size=200,
+        pop_size=50,
         maximize=False,
         num_elites=5)
 
     best = max(final_pop)
+
+    print best.candidate
+
+    pipeline = train_candidate(best.candidate, train_X, train_y)
+    test_individual(pipeline, validate_X, validate_y, display=True)
