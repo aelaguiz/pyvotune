3a7625811f861fec8359def114b31067064cf9cb
aelaguiz@gmail.com <aelaguiz@gmail.com>
2013-01-02 05:52:25 -0600

    Upped grid size

--- samples/mnist/globals.sh
+++ samples/mnist/globals.sh
@@ -2,7 +2,7 @@
 
 CLUSTER='lark'
 CLUSTER_USER='ubuntu'
-NODES="node001 node002 node003 node004"
+NODES="node001 node002 node003 node004 node005 node006 node007 node008 node009"
 
 INSPYRED_GIT=https://github.com/aelaguiz/inspyred.git
 PYVOTUNE_GIT=https://github.com/aelaguiz/pyvotune.git
@@ -30,7 +30,7 @@ NUM_SAMPLES=42000
 CROSSOVER_RATE=0.5
 MUTATION_RATE=0.3
 NEIGHBORHOOD_SIZE=2
-GRID_SIZE=22
+GRID_SIZE=32
 NUM_WORKERS=64
 EVAL_TIMEOUT=300
 MAX_LENGTH=5
