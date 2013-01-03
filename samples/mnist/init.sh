ec9c7adefb1f26cc444ec7be674d264f613cd2b7
aelaguiz@gmail.com <aelaguiz@gmail.com>
2013-01-02 19:36:27 -0600

    Perms

--- samples/mnist/init.sh
+++ samples/mnist/init.sh
@@ -6,7 +6,7 @@ source `dirname $0`/push.sh
 starcluster sshmaster $CLUSTER "$BASE_DIR/$MASTER_SETUP_ROOT_SCRIPT"
 starcluster sshmaster $CLUSTER -u $CLUSTER_USER "$BASE_DIR/$MASTER_SETUP_SCRIPT"
 
-starcluster sshnode $CLUSTER node001 -u $CLUSTER_USER "$BASE_DIR/$VENV_SETUP_SCRIPT"
+starcluster sshnode $CLUSTER node001 -u $CLUSTER_USER "$PYVOTUNE_DIR/$VENV_SETUP"
 
 #for i in $NODES
 #do
