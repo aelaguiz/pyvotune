29d44cd9e9836cd5b25966c627bc5be3f7709bd7
aelaguiz@gmail.com <aelaguiz@gmail.com>
2013-01-05 17:19:31 -0600

    Checkpoint

--- samples/mnist/start_cluster.sh
+++ samples/mnist/start_cluster.sh
@@ -2,14 +2,18 @@
 
 source `dirname $0`/globals.sh
 
-#source `dirname $0`/stop_cluster.sh
-
 starcluster sshmaster $CLUSTER -u $CLUSTER_USER "cd $PYVOTUNE_DIR && git pull"
+
+echo "Killing master"
+starcluster sshmaster $CLUSTER "tmux kill-session -t master"
+
+echo "Flushing master redis"
+starcluster sshmaster $CLUSTER "redis-cli flushall"
 
 echo "Starting master..."
 starcluster sshmaster $CLUSTER -u $CLUSTER_USER "tmux new -d -s master $PYVOTUNE_DIR/$START_MASTER"
 
-echo "Pausing"
-sleep 5
+#echo "Pausing"
+#sleep 5
 
-`dirname $0`/start_nodes.sh
+#`dirname $0`/start_nodes.sh
